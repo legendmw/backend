b06e493d4470935d3e8d3a7057c16f2197b8fd31
legendmw <brianmtonga592@gmail.com>
2024-08-11 16:25:26 +0200

    prod

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.use('/images', express.static('upload/images'));
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://backend-production-5954.up.railway.app:${port}/images/${req.file.filename}`
     });
 });
 
