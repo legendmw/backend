cf0171ab9d92eda8a17709102523252ae5df67b6
legendmw <brianmtonga592@gmail.com>
2024-08-11 21:00:10 +0200

    prod

--- index.js
+++ index.js
@@ -200,52 +200,56 @@ const Users = mongoose.model('Users', {
     }
 });
 
-app.post('/signup', async (req, res) => {
-    let check = await Users.findOne({ email: req.body.email });
+app.post('/signup',async (req,res)=>{
+
+    let check = await Users.findOne({email:req.body.email});
     if (check) {
-        return res.status(400).json({ success: false, errors: "existing user found with same email address" });
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
     }
     let cart = {};
-    for (let i = 0; i < 300; i++) {
-        cart[i] = 0;
+    for (let i = 0; i < 300; i++){
+        cart[i]=0;
     }
     const user = new Users({
-        name: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        cartData: cart,
-    });
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
 
     await user.save();
 
     const data = {
-        user: {
-            id: user.id
+        user:{
+            id:user.id
         }
-    };
+    }
 
-    const token = jwt.sign(data, process.env.JWT_SECRET);
-    res.json({ success: true, token });
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
 });
 
-app.post('/login', async (req, res) => {
-    let user = await Users.findOne({ email: req.body.email });
-    if (user) {
+app.post('/login',async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if (user){
         const passCompare = req.body.password === user.password;
-        if (passCompare) {
+        if (passCompare){
             const data = {
-                user: {
-                    id: user.id
+                user:{
+                    id:user.id
                 }
-            };
-            const token = jwt.sign(data, process.env.JWT_SECRET);
-            res.json({ success: true, token });
-        } else {
-            res.json({ success: false, errors: "Wrong Password" });
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({success:true,token});
         }
-    } else {
-        res.json({ success: false, errors: "Wrong Email Id" });
+        else{
+            res.json({success:false,errors:"Wrong Password"});
+        }
     }
+    else{
+        res.json({success:false,errors:"Wrong Email Id"});
+    }
+
 });
 
 app.get('/newcollections', async (req, res) => {
